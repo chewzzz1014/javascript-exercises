94ee9b8f11bb47c2178d1bff30cf8ac04e14083a
chewzzz <chewziqing@gmail.com>
2022-08-03 19:47:08 +0800

    Complete q1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+   let n = '';
+   for (let i=str.length-1; i>=0; i--)
+      n += str[i];
+  return n;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, args) {
+  let arr1  = [];
+  for (let e in args)
+      arr1.concat(arr.filter(ele => ele!=e));
+  return arr1;
 };
 
 // Do not edit below this line
