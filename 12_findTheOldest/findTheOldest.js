fe24f15490317c324a0f3d55405b31ce28465106
chewzzz <chewziqing@gmail.com>
2022-08-11 17:39:50 +0800

    Complete Q12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let maxAge = getAge(arr[0]);
+    let maxIdx = 0;
 
+    for (let i=1; i<arr.length; i++){
+      if (getAge(arr[i])>maxAge)
+      {
+        maxAge = getAge(arr[i]);
+        maxIdx = i
+      }
+    }
+    return arr[maxIdx].name;
 };
+
+function getAge(obj){
+  if (!obj?.yearOfDeath)
+    return (new Date().getFullYear()) -  obj.yearOfBirth;
+  else
+    return obj.yearOfDeath - obj.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
